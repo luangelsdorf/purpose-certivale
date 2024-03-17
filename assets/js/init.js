cd9c5cbf9dd2e05a0ad5908cab2a0f4535bb92f5
luangelsdorf <luanferreira2136@gmail.com>
2024-03-16 21:31:37 -0300

    Navbar component & init script

---
+++ assets/js/init.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  const sidenavReq = await fetch('/components/sidenav.html');
+  const sidenav = await sidenavReq.text();
+  document.querySelector('.main-content').insertAdjacentHTML('beforebegin', sidenav);
+
+  const footerReq = await fetch('/components/footer.html');
+  const footer = await footerReq.text();
+  document.querySelector('.page-content').insertAdjacentHTML('afterend', footer);
+
+  const navbarReq = await fetch('/components/navbar.html');
+  const navbar = await navbarReq.text();
+  document.querySelector('.page-content').insertAdjacentHTML('beforebegin', navbar);
+})
